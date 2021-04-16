4bca7367c46208bea5d2f8479200e4dd9cdd9067
Mcebo <samuel.mcebo@gmail.com>
2021-04-16 11:10:04 +0200

    Okay

--- js/greet-logic.js
+++ js/greet-logic.js
@@ -1,46 +1,85 @@
-function greetLanguageRadio() {
-    
 
-    var ListOfNames = []
+var nameList = {}
+var greetingsCounter = 0
+
+function greetLanguageRadio() {
+
     var languMessage = ''
     var person = ''
 
-    function setGreet(langu, per) {
-        if (langu === 'spanish'){
+    
+    if (localStorage['numberNames']) {
+        greetingsCounter = Number(localStorage['numberNames'])
+    } 
+    
+    if (typeof localStorage['NamesStored'] !== 'undefined'){
+        nameList = JSON.parse(localStorage.getItem('NamesStored'))
+    }
+
+    function langRun(la, per) {
+        if (la == 'spanish'){
             languMessage = 'Hola, '
-        } if (langu === 'isiZulu'){
+        } if (la == 'isiZulu'){
             languMessage = 'Sawubona, '
-        } if (langu === 'english') {
+        } if (la == 'english'){
             languMessage = 'Hello, '
         }
         person = per
+        // return languMessage + person
     }
 
-    function listPerson(name) {
-        ListOfNames = name
-        if (!ListOfNames.includes(person)){
-            ListOfNames.push(person)
-            return ListOfNames
-        }
-        return ListOfNames
-    }
-
-    function getName() {
-        if (ListOfNames[person] === undefined){
-            return person
+    function checkErrors() {
+        if (person == ''){
+            return 'Please pass a string/Name of a person'
+        } else if (languMessage == '' || languMessage == undefined){
+            return 'Check one of the languages using the Radio'
         }
     }
 
-    function greetPerson() {
-        if (ListOfNames[person] === undefined){
-            return languMessage + person
-        }
+    //Converting the first Letter
+    function capFirstLetter(str) {
+        return str.charAt(0).toUpperCase() + str.slice(1)
     }
+
+    //Adding to the object
+    function nameLists(name) {
+        var comb = languMessage + person
+        if (name != ''){
+            if (nameList[name] === undefined){
+                greetingsCounter++
+                localStorage.setItem('numberNames', greetingsCounter)  
+                nameList[name] = 1
+                localStorage.setItem('NamesStored', JSON.stringify(nameList))  
+            }
+            return comb
+        } else {
+            alert('Please Pass a String')
+            return "You can't leave it empty!"
+        }
+        
+    }
+
+    function getCounter() {
+        return greetingsCounter
+    }
+
+    function getNameList() {
+        return nameList
+    }
+
+    function reset() {
+        localStorage.clear()
+        location.reload()
+    }
+
 
     return {
-        setGreet,
-        getName,
-        greetPerson,
-        listPerson,
+        reset: reset,
+        langRun : langRun,
+        checkErrors : checkErrors,
+        nameLists : nameLists,
+        getCounter : getCounter,
+        getNameList : getNameList,
+        capFirstLetter : capFirstLetter
     }
 }
--- js/greet.js
+++ js/greet.js
@@ -3,57 +3,30 @@ var greetText = document.querySelector('.greetText')
 var greetTextArea = document.querySelector('.greetTextArea')
 var count = document.querySelector('.numberCounter')
 var clearButton = document.querySelector('.clearButton')
-var greet = greetLanguageRadio()
-var greetedNames = []
+var errors = document.querySelector('.errors')
+var greetLang = greetLanguageRadio()
 
-// greetedNames = localStorage.getItem('names')
-if (typeof localStorage['Names'] !== 'undefined') {
-    greetedNames = localStorage.getItem('Names')
-}
-
-greetText.innerHTML = greet.greetPerson()
-count.innerHTML = greetedNames.length
+count.innerHTML = greetLang.getCounter()
 
 
 greetButton.addEventListener('click', function(){
     var checkedlanguageElem = document.querySelector("input[name='languageRadio']:checked")
     var namePerson = greetTextArea.value.trim()
-    var checkedlanguage
 
     if (checkedlanguageElem){
-        checkedlanguage = checkedlanguageElem.value
-    } if (!checkedlanguageElem && namePerson ==''){
-        alert('Enter a name and Select a language!')
-    } if (!checkedlanguageElem){
-        alert('Select a Langugae')
-    } if (namePerson ==''){
-        alert('Enter a name')
+        var checkedlanguage = checkedlanguageElem.value
+        var namePerson1 = greetLang.capFirstLetter(namePerson)
+
+        greetLang.langRun(checkedlanguage, namePerson1)
+        greetText.innerHTML = greetLang.nameLists(namePerson1)
+        count.innerHTML = greetLang.getCounter()
+    } else {
+        alert('Language not Checked')
+        greetText.innerHTML = 'Plase Select the Language'
     }
-
-    console.log(namePerson)
-    if (!(checkedlanguage === undefined) && !(greetedNames.includes(namePerson))){
-        greet.setGreet(checkedlanguage, namePerson)
-        greetedNames = greet.listPerson(greetedNames)
-
-        // var gNames = JSON.stringify(greetedNames)
-
-        localStorage.setItem('Names', greetedNames )
-
-        greetText.innerHTML = greet.greetPerson()
-        count.innerHTML = greetedNames.length
-
-        namePerson = ''
-    }
-    else {
-        greet.setGreet(checkedlanguage, namePerson)
-        greetText.innerHTML = greet.greetPerson()
-    }
-
-    console.log(greetedNames)
-    
+    console.log(greetLang.getNameList())
 })
 
 clearButton.addEventListener('click', function(){
-    localStorage.clear()
-    location.reload()
+    greetLang.reset()
 })
